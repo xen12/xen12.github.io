e9acc13a61969c8cd5a89a94643e1b0cf35e3389
Giobu <gus.xen.12@gmail.com>
2024-12-22 23:25:04 -0400

    Deploy commit

---
+++ chunk-LUQBHAZS.js
@@ -0,0 +1,2 @@
+import{i as o,qb as t}from"./chunk-FHH2HRBQ.js";import"./chunk-2H7K6JAD.js";var e=`<p>about works!</p>\r
+`;var p="";var r=class{};r=o([t({selector:"app-about",standalone:!0,imports:[],template:e,styles:[p]})],r);export{r as AboutComponent};
---
+++ chunk-VMZ2MSSW.js
@@ -0,0 +1,2 @@
+import{i as o,qb as t}from"./chunk-FHH2HRBQ.js";import"./chunk-2H7K6JAD.js";var e=`<p>projects works!</p>\r
+`;var r="";var p=class{};p=o([t({selector:"app-projects",standalone:!0,imports:[],template:e,styles:[r]})],p);export{p as ProjectsComponent};
